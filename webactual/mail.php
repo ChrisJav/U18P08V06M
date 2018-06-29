1b9d2a732f7c13bf3405993452951c9586c58e1a
miawlfs <35786328+miawlfs@users.noreply.github.com>
2018-06-29 11:12:49 -0500

    prueva de correo  aun no queda

---
+++ webactual/mail.php
@@ -0,0 +1,11 @@
+<?php
+
+$destino= "saulperez.sistemas@gmail.com";
+$nombre=$_Post["nombre"];
+$email=$_Post["correro"];
+$subject=$_Post["subject"];
+$mensage=$_Post["mensaje"];
+$contenido="Embre: ". $nombre ."\nEmail: ".$email."n\Subject:".$subject."\nMensaje".$mensage;
+mail($destino,"contanto",$contenido);
+header("Location:gracias.html");
+ ?>
