8e7a07df4cea24d73e3d135949e0b8c563ebaac0
miawlfs <35786328+miawlfs@users.noreply.github.com>
2018-06-30 21:02:50 -0500

    prueva lisgta
    solo falta arreglar detalles

--- webactual/mail.php
+++ webactual/mail.php
@@ -1,24 +1,13 @@
 <?php
 
-<<<<<<< HEAD
 $destino= "saulperez.sistemas@gmail.com";
 $name="$_REQUEST[name]";
 $email="$_REQUEST[email]";
 $subject="$_REQUEST[subject]";
 $message="$_REQUEST[message]";
-$contenido="name : ".$name ."\nEmail : ". $email ."n\Subject :". $subject ."\nMessaje" .$message;
+$contenido="name :  ".$name ." \nEmail : ". $email ." \nSubject : ". $subject ." \nMessaje  " .$message;
 mail($destino,"contanto",$contenido);
 header("Location:gracias.html");
 
 
-=======
-$destino= "triboquimia@gmail.com";
-$nombre=$_Post["nombre"];
-$email=$_Post["correro"];
-$subject=$_Post["subject"];
-$mensage=$_Post["mensaje"];
-$contenido="Embre: ". $nombre ."\nEmail: ".$email."n\Subject:".$subject."\nMensaje".$mensage;
-mail($destino,"contanto",$contenido);
-header("Location:gracias.html");
->>>>>>> 181b0e50000116f124a317759b7008e82edbeb3e
- ?>
+?>
